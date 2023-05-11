b15b6e7f1ec026fb0796b9c091e66cdcd28ef01d
Ted Conner <42025319+Tedconner23@users.noreply.github.com>
2023-05-11 16:22:54 -0700

    Create init.ps1

---
+++ init.ps1
@@ -0,0 +1,8 @@
+# Set the PYTHONUTF8 environment variable
+$env:PYTHONUTF8 = 1
+
+# Install the requirements from the requirements.txt file
+pip install -r requirements.txt
+
+# Run the main.py script
+python main.py
